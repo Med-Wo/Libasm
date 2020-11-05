5d6a17975832c78e51890935e6f7a3a21dc08922
mravily <mravily@student.42.fr>
2020-11-05 18:51:41 +0100

    Fix ft_strlen.s

--- ft_strlen.s
+++ ft_strlen.s
@@ -7,16 +7,16 @@
 
 			section .text					; Debut des instruction de la fonction
 ft_strlen: 									; RDI == const char *s
+			xor rax, rax					; Initialisation du registe RAX (index) a 0
 			cmp rdi, 0						; Verifie si la str envoyer en params(RDI) n' est pas null
 			je return						; Si RDI == NULL quit le programme et renvoi 0
-			xor rax, rax					; Initialisation du registe RAX (index) a 0
-			jmp loop						; Jump dans le label compare
+			jmp .loop						; Jump dans le label .loop
 
-loop:										; Boucle de parcours de la chaine
+.loop:										; Boucle de parcours de la chaine
 			cmp byte [rdi + rax], 0			; Compare si le byte rdi a l'index rax est egale a \0 "$"
 			je return						; Si rdi[rax] est egual a \0 saute au label return
-			inc rax							; Incremente le registre rsi qui sert d'index
-			jmp compare						; Jump au debut du label compare
+			inc rax							; Incremente le registre rax qui sert d'index
+			jmp .loop						; Jump au debut du label .loop
 
 return:
 			ret								; Return le registre rax contenant la taille de la chaine
